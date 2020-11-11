f7106ced5b3027886781b0cf4e0be8cbfb9da070
Eduard Robu <robu.edi@gmail.com>
2020-11-11 21:01:41 +0200

    init scripts logging, npm to dockerfile, image reusability by naming added

--- docker/init.sh
+++ docker/init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# migrate
-php artisan migrate
+touch initial-script-output.txt
+
+# vendors
+composer update >> initial-script-output.txt 2>&1
+composer dump-autoload >> initial-script-output.txt 2>&1
 
 # fresh migration
-php artisan migrate:fresh --seed
+php artisan migrate:fresh --seed >> initial-script-output.txt 2>&1
+
+npm install >> initial-script-output.txt 2>&1
+
+npm run >> initial-script-output.txt 2>&1
+
+rm initial-script-output.txt
 
 exit 0
